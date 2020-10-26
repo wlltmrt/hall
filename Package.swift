61b0f24cdc79195a4d9c27ffeefa5abae3e21435
Wellington Marthas <well@dyntime.com>
2020-10-26 17:54:03 -0300

    Minimum iOS supported is 11

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ import PackageDescription
 let package = Package(
     name: "Hall",
     platforms: [
-        .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
+        .macOS(.v10_13), .iOS(.v11), .watchOS(.v4)
     ],
     products: [
         .library(
